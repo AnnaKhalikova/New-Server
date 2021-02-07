58f88bf9e07181bef952c395c1dc29b91446bba2
AnnaKhalikova <62241994+AnnaKhalikova@users.noreply.github.com>
2021-02-07 16:21:28 +0300

    Update serverRequest.js

--- frontend/libs/js/serverRequest.js
+++ frontend/libs/js/serverRequest.js
@@ -25,7 +25,7 @@ document.addEventListener("DOMContentLoaded", function () {
                         content.classList.add('hide');
 
                         subtitle.innerHTML = "The result of checking the text for uniqueness";
-                        serverAnswer.innerText = `Your text is ${request.responseText} % unique`;
+                        serverAnswer.innerHTML = `Your text is ${request.responseText} % unique`;
                     }
 
                     if (event.target.id === "back") {
