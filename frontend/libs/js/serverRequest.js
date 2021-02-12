1c4f194d85a7c6ef1c9747c8f5784a5c580b97b0
AnnaKhalikova <hanna.Khalikova@gmail.com>
2021-02-12 22:05:28 +0300

    new front update

--- frontend/libs/js/serverRequest.js
+++ frontend/libs/js/serverRequest.js
@@ -1,54 +1,37 @@
 document.addEventListener("DOMContentLoaded", function () {
+    let payload = 'name=' + encodeURIComponent(document.getElementById('value').value)
 
-    const sendButton = document.getElementById('send') // button send
-    sendButton.addEventListener('click', submitForm () {
-        let payload = 'name=' + encodeURIComponent(document.getElementById('value').value)
+    const request = new XMLHttpRequest()
+    request.open('POST', 'http://34.86.152.212:8080/cgi-bin/script.cgi', true)
 
-        const request = new XMLHttpRequest()
-        request.open('POST', 'http://34.86.152.212:8080/cgi-bin/script.cgi', true)
+    request.addEventListener('readystatechange', function () {
 
-        request.addEventListener('readystatechange', function () {
+        if ((request.readyState === 4) && (request.status === 200)) {
+            function onSubmitForm() {
+                event.preventDefault()
+                resultContent.classList.remove('hide');
+                resultContent.classList.add('show');
 
-            if ((request.readyState === 4) && (request.status === 200)) {
-                const form = document.forms.submitAI
-                const result = form.querySelector('#result');
-              
-                // all dynamic elements should be located inside one form
-                // in that case all we need to copy paste the same code of form from html
-                // in every new project, just ctrl+c / ctrl+v
-                
-                const container = document.querySelector('#container');
-                const result = document.querySelector('#result');
-                const content = document.querySelector('#content');
-                const subtitle = document.querySelector("#subtitle");
-                const serverAnswer = document.querySelector("#serverAnswer");
+                testContent.classList.remove('show');
+                testContent.classList.add('hide');
 
-                container.addEventListener('click', event => {
-                    if (event.target.id === "send") {
-                        result.classList.remove('hide');
-                        result.classList.add('show');
+                subtitle.innerHTML = "The result of checking the text for uniqueness";
+            }
 
-                        content.classList.remove('show');
-                        content.classList.add('hide');
+            function returnBack() {
+                testContent.classList.remove('hide');
+                testContent.classList.add('show');
 
-                        subtitle.innerHTML = "The result of checking the text for uniqueness";
-                        serverAnswer.innerHTML = `Your text is ${request.responseText} percents unique`;
-                    }
+                resultContent.classList.remove('show');
+                resultContent.classList.add('hide');
 
-                    if (event.target.id === "back") {
-                        result.classList.remove('show');
-                        result.classList.add('hide');
-
-                        content.classList.remove('hide');
-                        content.classList.add('show');
-
-                        subtitle.innerHTML = "Intelligent App for text processing";
-                    }
-                })
-            } 
-        })
-
-        request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded')
-        request.send(payload)
+                subtitle.innerHTML = "Intelligent App for text processing";
+            }
+        } else {
+            alert("Something wrong!")
+        }
     })
+
+    request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded')
+    request.send(payload)
 })
