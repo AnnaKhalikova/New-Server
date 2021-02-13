f4dac0dc5bd64b29e678566ab8f115ba56135f04
AnnaKhalikova <62241994+AnnaKhalikova@users.noreply.github.com>
2021-02-13 16:07:35 +0300

    Update serverRequest.js

--- src/serverRequest.js
+++ src/serverRequest.js
@@ -1,37 +1,19 @@
 document.addEventListener("DOMContentLoaded", function () {
-    let payload = 'name=' + encodeURIComponent(document.getElementById('value').value)
 
-    const request = new XMLHttpRequest()
-    request.open('POST', 'http://34.86.152.212:8080/cgi-bin/script.cgi', true)
+    const sendButton = document.getElementById('send') // button send
+    sendButton.addEventListener('click', function () {
+        let payload = 'name=' + encodeURIComponent(document.getElementById('value').value)
 
-    request.addEventListener('readystatechange', function () {
+        const request = new XMLHttpRequest()
+        request.open('POST', 'http://34.86.152.212:8080/cgi-bin/script.cgi', true)
 
-        if ((request.readyState === 4) && (request.status === 200)) {
-            function onSubmitForm() {
-                event.preventDefault()
-                resultContent.classList.remove('hide');
-                resultContent.classList.add('show');
+        request.addEventListener('readystatechange', function () {
+            console.log(parseInt(request.responseText.replace(/[^\d]/g, '')));
+            document.querySelector("#img").classList.add('opacity');
+            document.querySelector("#result").innerHTML = `${parseInt(request.responseText.replace(/[^\d]/g, ''))} % unique`;
+        })
 
-                testContent.classList.remove('show');
-                testContent.classList.add('hide');
-
-                subtitle.innerHTML = "The result of checking the text for uniqueness";
-            }
-
-            function returnBack() {
-                testContent.classList.remove('hide');
-                testContent.classList.add('show');
-
-                resultContent.classList.remove('show');
-                resultContent.classList.add('hide');
-
-                subtitle.innerHTML = "Intelligent App for text processing";
-            }
-        } else {
-            alert("Something wrong!")
-        }
+        request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded')
+        request.send(payload)
     })
-
-    request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded')
-    request.send(payload)
 })
