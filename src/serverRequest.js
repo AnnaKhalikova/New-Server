0801af492b8f41f937de34b489ef6ab442e218f1
AnnaKhalikova <62241994+AnnaKhalikova@users.noreply.github.com>
2021-02-16 12:44:52 +0300

    Update serverRequest.js

--- src/serverRequest.js
+++ src/serverRequest.js
@@ -6,12 +6,14 @@ document.addEventListener("DOMContentLoaded", function () {
 
         const request = new XMLHttpRequest()
         request.open('POST', 'http://34.86.152.212:8080/cgi-bin/script.cgi', true)
-        if ((request.readyState === 4) && (request.status === 200)) {
-            request.addEventListener('readystatechange', function () {
-            console.log(parseInt(request.responseText.replace(/[^\d]/g, '')));
-            document.querySelector("#img").classList.add('opacity');
-            document.querySelector("#result").innerHTML = `${parseInt(request.responseText.replace(/[^\d]/g, ''))} % unique`;
-        })}
+        
+        request.addEventListener('readystatechange', function () {
+            if ((request.readyState === 4) && (request.status === 200)) {
+                console.log(parseInt(request.responseText.replace(/[^\d]/g, '')));
+                document.querySelector("#img").classList.add('opacity');
+                document.querySelector("#result").innerHTML = `${parseInt(request.responseText.replace(/[^\d]/g, ''))} % unique`;
+            }
+        })
 
         request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded')
         request.send(payload)
